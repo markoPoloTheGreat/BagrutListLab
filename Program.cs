1cfb887d4d4136c87f024a145dccf6d6c72c38f1
User <User@DESKTOP-5PR9RUE>
2025-03-30 10:02:19 +0300

    gfygtyg

--- Program.cs
+++ Program.cs
@@ -4,66 +4,77 @@ public class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Node<int> lst = new Node<int>(5);//יצירת חוליה ראשונה ששם הרשימה מצביע עליה
-                                         //null בעת היצירה החוליה היחידה מצביעה על
-        Console.WriteLine(lst);
-        Node<int> currNode = new Node<int>(7, lst);//יצירת חוליה נוספת, שמוצבת לפני ליסט ומצביעה
-                                                   //על החוליה הראשונה של ליסט
-        lst = currNode;//גורמים למצביע ליסט להצביע על החוליה שהוספנו בתחילת הרשימה
-        Console.WriteLine(lst);
-        currNode = new Node<int>(9, lst);
-        lst = currNode;
-        currNode = new Node<int>(-2, lst);
-        lst = currNode;
-        currNode = new Node<int>(10, lst);
-        lst = currNode;
-        currNode = new Node<int>(22, lst);
-        lst = currNode;
-        currNode = new Node<int>(1, lst);
-        lst = currNode;
-        currNode = new Node<int>(-1, lst);
-        lst = currNode;
-        currNode = new Node<int>(-9, lst);
-        lst = currNode;
-        lst = new Node<int>(-55, lst);//צורת כתיבה שמאפשרת להוסיף חוליה בראש שרשרת החוליות ולהעביר אתיה את מצביע ראש הרשימה
-        Console.WriteLine(lst);
-
-        Console.WriteLine(lst.GetValue());//הדפסת הערך של החוליה הראשונה ברשימה
-
-        Console.WriteLine(lst.GetNext().GetValue());//הדפסת הערך של החוליה השנייה ברשימה
-
-        currNode = lst.GetNext();//הצבעה על החוליה השניה בשרשרת החוליות
-
-        currNode = currNode.GetNext();//קידום ההצבעה של החוליות הנוכחית באחת
-
-        //שילוב חוליה חדשה בשרשרת אחרי חוליה נתונה
-        //נהנה למשל שרשרת חוליות מסודרת בסדר עולה
-        Node<int> lst1 = new Node<int>(10);// 10-->null
-        lst1 = new Node<int>(8, lst1);// 8-->10-->null
-        lst1 = new Node<int>(6, lst1);// 6-->8-->10-->null
-        lst1 = new Node<int>(4, lst1);// 4-->6-->8-->10-->null
+        Node<int> lst1 = new Node<int>(10);
+        Node<int> currNode= new Node<int>(9,lst1);
+        lst1 = currNode;
+        currNode= new(8,lst1);
+        lst1 = currNode;
+        currNode = new(6,lst1);
+        lst1 = currNode;
+        currNode = new(5,lst1);
+        lst1 = currNode;
+        currNode = new(4,lst1);
+        lst1 = currNode;
+        currNode = new(3,lst1);
+        lst1 = currNode;
+        currNode = new(2,lst1);
+        lst1 = currNode;
+        Console.WriteLine("sum is:"+SumLinked(lst1));
+        Console.WriteLine("amount of evens:"+ EvenInList(lst1));
+        InsertSortedList(lst1,7);
         Console.WriteLine(lst1);
 
-        //ניצור חולייה חדשה ונכניס אותה לשרשרת לפי ערך המספר
-        Node<int> NewNode = new Node<int>(12);// 7-->null
-        currNode = lst1;//הצבעה על החוליה הראשונה בשרשרת
-        if (currNode.GetValue() > NewNode.GetValue())
+    }
+    public static int SumLinked(Node<int> first)
+    {
+        Node<int> pos = first;
+        int sum = 0;
+        while (pos != null)
         {
-            NewNode.SetNext(lst1);//הצבת החוליה החדשה לפני החוליה הראשונה בשרשרת
-            lst1 = NewNode;//ההצבת שם השרשרת על המספר החדש שנוסף בראשה
+            sum += pos.GetValue();
+            pos = pos.GetNext();
         }
-        else
+        return sum;
+    }
+    public static int EvenInList(Node<int> first)
+    {
+        Node<int> pos = first;
+        int evens = 0;
+        while (pos != null)
         {
-            while (currNode.HasNext() && currNode.GetNext().GetValue() < NewNode.GetValue())
+            if (pos.GetValue() % 2 == 0)
             {
-                currNode = currNode.GetNext();//כל עוד יש חוליה עוקבת והערך שלה קטן מהחוליה החדשה מתקדמים חוליה
+                evens++;
             }
-            NewNode.SetNext(currNode.GetNext());//הצבת החוליה החדשה לפני החוליה העוקבת את הנוכחית
-            currNode.SetNext(NewNode);//הצבת החוליה החדשה אחרי החוליה הנוכחית        
+            pos = pos.GetNext();
         }
-        Console.WriteLine(lst1);
+        return evens;
+    }
+    public static int BiggestInList (Node<int> first)
+    {
+        Node<int> pos = first;
+        int biggest=pos.GetValue();
+        while (pos != null)
+        {
+            if (pos.GetValue() > biggest)
+            {
+                biggest = pos.GetValue();
+            }
+            pos = pos.GetNext();
+        }
+        return biggest;
 
+    }
+    public static void InsertSortedList(Node<int> first,int hadash)
+    {
+        Node<int> pos= first;
+        Node<int> insert = new(hadash);
+        while (pos.GetValue() < insert.GetValue())
+        {
+            pos = pos.GetNext();
+        }
+        insert.SetNext(pos.GetNext());
+        pos.SetNext(insert);
     }
 
 }
