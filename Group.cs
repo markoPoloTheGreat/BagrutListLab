956c507706fbd6840f8e4976fd9f18b5c881f382
markoPoloTheGreat <markyaholon@gmail.com>
2025-04-20 07:17:25 +0300

    בדגכד

---
+++ Group.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BagrutListLab
+{
+    public class Group
+    {
+        private string[] namesArr;
+        private int kamut;
+        public Group(string[] namesArr,int kamut)
+        {
+            this.namesArr = namesArr;
+            this.kamut = kamut;
+        }
+        public int GetKamut()
+        {
+            return kamut;
+        }
+        public string[] GetNamesArr()
+        {
+            return namesArr;
+        }
+        public bool IsIncluded(string name)
+        {
+            for (int i = 0; i < this.kamut; i++)
+            {
+                if (name == this.namesArr[i])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,33 +6,33 @@ public class Program
     private static void Main(string[] args)
     {
         Node<int> lst1 = new Node<int>(10);
-        Node<int> currNode= new Node<int>(9,lst1);
+        Node<int> currNode = new Node<int>(9, lst1);
         lst1 = currNode;
-        currNode= new(8,lst1);
+        currNode = new(8, lst1);
         lst1 = currNode;
-        currNode = new(6,lst1);
+        currNode = new(6, lst1);
         lst1 = currNode;
-        currNode = new(5,lst1);
+        currNode = new(5, lst1);
         lst1 = currNode;
-        currNode = new(4,lst1);
+        currNode = new(4, lst1);
         lst1 = currNode;
-        currNode = new(3,lst1);
+        currNode = new(3, lst1);
         lst1 = currNode;
-        currNode = new(2,lst1);
+        currNode = new(2, lst1);
         lst1 = currNode;
-        Console.WriteLine("sum is:"+SumLinked(lst1));
-        Console.WriteLine("amount of evens:"+ EvenInList(lst1));
-        InsertSortedList(lst1,7);
+        Console.WriteLine("sum is:" + SumLinked(lst1));
+        Console.WriteLine("amount of evens:" + EvenInList(lst1));
+        InsertSortedList(lst1, 7);
         lst1 = new(1, lst1);
         Console.WriteLine(lst1);
-        Console.WriteLine("size is: "+ SizeLst(lst1));
+        Console.WriteLine("size is: " + SizeLst(lst1));
         Node<int> lst2 = new Node<int>(6);
         lst2 = new(0, lst2);
         lst2 = new(8, lst2);
         lst2 = new(1, lst2);
         lst2 = new(1, lst2);
         lst2 = new(1, lst2);
-        Console.WriteLine("is it perf: "+CheckPerfLst(lst2));
+        Console.WriteLine("is it perf: " + CheckPerfLst(lst2));
         Ranges r1 = new(1, 5);
         Ranges r2 = new(8, 10);
         Ranges r3 = new(11, 15);
@@ -40,7 +40,7 @@ public class Program
         lst3 = new(r2, lst3);
         lst3 = new(r1, lst3);
         Console.WriteLine("hi");
-        Console.WriteLine("is 8 in the ranges: "+CheckInRange(lst3,8));
+        Console.WriteLine("is 8 in the ranges: " + CheckInRange(lst3, 8));
         CreateRangeForNum(lst3, 6);
         Console.WriteLine(lst3);
         Node<int> lst4 = null;
@@ -76,10 +76,10 @@ public class Program
         }
         return evens;
     }
-    public static int BiggestInList (Node<int> first)
+    public static int BiggestInList(Node<int> first)
     {
         Node<int> pos = first;
-        int biggest=pos.GetValue();
+        int biggest = pos.GetValue();
         while (pos != null)
         {
             if (pos.GetValue() > biggest)
@@ -123,15 +123,15 @@ public class Program
     {
         Node<int> last = first;
         Node<int> pos = first;
-        for(int i = 0;i<SizeLst(first)/2; i++)
+        for (int i = 0; i < SizeLst(first) / 2; i++)
         {
             last = last.GetNext();
         }
-        for (int i = 0; i<SizeLst(first)/2; i++)
+        for (int i = 0; i < SizeLst(first) / 2; i++)
         {
-            for(int j = 0; j<SizeLst(first)/2; j++)
+            for (int j = 0; j < SizeLst(first) / 2; j++)
             {
-                if(last.GetValue() < pos.GetValue())
+                if (last.GetValue() < pos.GetValue())
                 {
                     return false;
                 }
@@ -156,9 +156,9 @@ public class Program
         }
         return false;
     }
-    public static void CreateRangeForNum(Node<BagrutListLab.Ranges> first,int num)
+    public static void CreateRangeForNum(Node<BagrutListLab.Ranges> first, int num)
     {
-        Node<BagrutListLab.Ranges> pos=first;
+        Node<BagrutListLab.Ranges> pos = first;
         while (num > pos.GetNext().GetValue().GetHigh())
         {
             pos = pos.GetNext();
@@ -172,33 +172,33 @@ public class Program
         {
             upper = num + 5;
         }
-        else if((pos.HasNext()) && (pos.GetNext().GetValue().GetLow() <= 5 + num))
+        else if ((pos.HasNext()) && (pos.GetNext().GetValue().GetLow() <= 5 + num))
         {
-            upper = pos.GetNext().GetValue().GetLow()-1;
+            upper = pos.GetNext().GetValue().GetLow() - 1;
         }
-        else 
+        else
         {
             upper = num + 5;
         }
         int lower;
-        if ((pos.HasNext()) && (pos.GetNext().GetValue().GetLow() < num-5))
-        {
-            lower = num -5;
-        }
-        else if ((pos.HasNext()) && (pos.GetNext().GetValue().GetLow() >= num-5))
-        {
-            lower = pos.GetNext().GetValue().GetLow()-1;
-        }
-        else 
+        if ((pos.HasNext()) && (pos.GetNext().GetValue().GetLow() < num - 5))
         {
             lower = num - 5;
         }
-        if(num<pos.GetValue().GetLow())
+        else if ((pos.HasNext()) && (pos.GetNext().GetValue().GetLow() >= num - 5))
+        {
+            lower = pos.GetNext().GetValue().GetLow() - 1;
+        }
+        else
+        {
+            lower = num - 5;
+        }
+        if (num < pos.GetValue().GetLow())
         {
             upper = pos.GetValue().GetLow() - 1;
             lower = num - 5;
         }
-        Ranges hadashR=new(lower,upper);
+        Ranges hadashR = new(lower, upper);
         Node<BagrutListLab.Ranges> hadash = new(hadashR, pos.GetNext());
         pos.SetNext(hadash);
     }
@@ -217,5 +217,45 @@ public class Program
         newNode.SetNext(currNode.GetNext());
         currNode.SetNext(newNode);
         return lst;
+    }
+    public static int AvgGradeInLst(Node<Student> first)
+    {//עוברת על התלמידים ומחשבת את הממוצע שלהם
+        Node<Student> currNode = first;
+        int sum = 0;
+        int count = 0;
+        while (currNode != null)
+        {
+            sum += currNode.GetValue().GetGrade();
+            count++;
+            currNode = currNode.GetNext();
+        }
+        return sum / count;
+    }
+    public static Node<string> AllGroups(Group[] GroupArr)
+    {
+        Group minG = GroupArr[0];
+        for (int i = 1; i < GroupArr.Length; i++)//find the smallest group
+        {
+            if (GroupArr[i].GetKamut() < minG.GetKamut())
+            {
+                minG = GroupArr[i];
+            }
+        }
+        bool isInAll = true;
+        for (int i = 0; i < minG.GetKamut(); i++)
+        {
+            for (int j = 0; j < GroupArr.Length; j++)
+            {
+                if (!GroupArr[j].IsIncluded(minG.GetNamesArr()[i]))
+                {
+                    isInAll = false;
+                }
+
+            }
+            if (isInAll)
+            {
+
+            }
+        }
     }
 }
